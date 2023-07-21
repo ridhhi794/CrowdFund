26f09ab0c40229e1e782c7aebd8c0392ac241c7f
ridhhi794 <ridhhiaggarwal@Ridhhis-MacBook-Air.local>
2023-07-21 13:27:05 +0530

    initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Sepolia } from "@thirdweb-dev/chains";
+import { StateContextProvider } from "./context";
 import App from "./App";
 import "./index.css";
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <ThirdwebProvider activeChain={Sepolia}>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </ThirdwebProvider>
   </Router>
 );
